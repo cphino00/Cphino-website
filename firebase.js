f4d77af0397b33ad361454ba7c0c24f914bacc9c
cphino00 <cphino00@gmail.com>
2026-01-12 09:54:30 +0530

    Update and rename Firebase.js to firebase.js

--- firebase.js
+++ firebase.js
@@ -11,7 +11,6 @@ const firebaseConfig = {
   appId: "1:104788057588:web:dcb4aa93d28b903a35d221"
 };
 
-const app = initializeApp(firebaseConfig);
-
+export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
