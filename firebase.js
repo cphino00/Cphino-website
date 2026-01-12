4ae5cdb2f4c0e80539c000f2181f0f378cfe7a85
cphino00 <cphino00@gmail.com>
2026-01-12 09:38:08 +0530

    firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD...",
+  authDomain: "cphino-knowledgehub.firebaseapp.com",
+  projectId: "cphino-knowledgehub",
+  storageBucket: "cphino-knowledgehub.appspot.com",
+  messagingSenderId: "104788057588",
+  appId: "1:104788057588:web:dcb4aa93d28b903a35d221"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
